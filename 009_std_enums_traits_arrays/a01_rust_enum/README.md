7604e0d53c2b9e2b4ec51f0e0ba2f4e3f90b34e6
GYoung <ytok1108@aol.com>
2024-04-18 20:36:35 +0900

    index[1] 출력하기

---
+++ 009_std_enums_traits_arrays/a01_rust_enum/README.md
@@ -0,0 +1,10 @@
+# Result
+
+```bash
+
+This gem is worth 25
+This gem is worth 100
+This gem is worth 5
+This gem is worth 50
+```
+
---
+++ 009_std_enums_traits_arrays/a01_rust_enum/src/main.rs
@@ -0,0 +1,21 @@
+enum Gem {
+    Diamond,
+    Sapphire,
+    Ruby,
+    Topaz,
+    Onyx,
+    Jade,
+}
+
+fn main() {
+    let gems = [
+        (Gem::Onyx, 25.00),
+        (Gem::Diamond, 100.00),
+        (Gem::Topaz, 5.00),
+        (Gem::Onyx, 50.00),
+    ];
+
+    for gem in gems {
+        println!("This gem is worth {}", gem.1);
+    }
+}
